8a846ea9cf2dbe651a82642d778b7595a7ba1c15
Tyuiu.MarinenkovaKD@outlook.com <marinenkova05@gmail.com>
2023-11-25 11:47:01 +0500

    Task5

---
+++ Tyuiu.MarinenkovaKD.Sprint5.Task5.V28.Lib/DataService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.MarinenkovaKD.Sprint5.Task5.V28.Lib
+{
+    public class DataService : ISprint5Task5V28
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double res = 1;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                double min = int.MaxValue;
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    double number = double.Parse(line);
+                    if (number > 0 && number % 5 == 0 && number < min)
+                    {
+                        min = number;
+                    }
+
+                    
+                    for (int i = 2; i <= number; i++)
+                    {
+                        res *= i;
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.MarinenkovaKD.Sprint5.Task5.V28/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Tyuiu.MarinenkovaKD.Sprint5.Task5.V28.Lib;
+
+namespace Tyuiu.MarinenkovaKD.Sprint5.Task5.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила: Мариненкова К. Д. | ИСТНб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Чтение данных из текстового файла                                 *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнила: Мариненкова Кристина Дмитриевна | ИСТНб-23-1                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Найти факториал наименьшего положительного целого числа, которое делится*");
+            Console.WriteLine("* на 5, в файле.                                                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask5V28.txt";
+            Console.WriteLine("Данные находятся в файле: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
